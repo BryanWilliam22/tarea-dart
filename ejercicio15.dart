e0ff630a394c4e35bb66d72718dd8695e4714a95
Will <eate.bryanwilliam.alarcon.yu@unifranz.edu.bo>
2026-03-16 23:52:20 -0400

    Create ejercicio15.dart
    ejercicio15.dart

---
+++ ejercicio15.dart
@@ -0,0 +1,11 @@
+void main() {
+  List<double> notas = [70, 80, 90];
+  double suma = 0;
+
+  for (double n in notas) {
+    suma += n;
+  }
+
+  double prom = suma / notas.length;
+  print("Promedio: $prom");
+}
